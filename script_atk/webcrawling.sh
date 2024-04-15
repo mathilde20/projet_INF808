fa582df9ea3c4dabf20b9ca9ee8112aea24d15c3
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-04-14 21:35:16 -0400

    feat: added the attack dockerfile and services

--- script_atk/webcrawling.sh
+++ script_atk/webcrawling.sh
@@ -3,5 +3,5 @@ if ["$1" == ""]
 then
 	echo "Usage: ./webcrawling [IP] [dico.txt]"
 else
-	gobuster dir -u http://$1/dvwa/ -w $2 -x txt,php,cgi,html,pdf
+	gobuster dir -u $1 -w $2 -x txt,php,cgi,html,pdf
 fi
